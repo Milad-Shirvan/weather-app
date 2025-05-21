99724347ebe0bae4720c3156116e5eaa9eb2875b
Milad-Shirvan <miladmagic3@gmail.com>
2025-05-21 16:58:26 +0200

    weather app
    just a simple weather app with api

---
+++ .history/index_20250113095458.js
@@ -0,0 +1,38 @@
+const apikey = process.env.WEATHER_API_KEY;
+const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox=document.querySelector(".search input");
+const searchbtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+async function checkweather(city){
+    const response =await fetch(apiurl +city+`&appid=${apikey}`);
+    
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }else{
+        var data = await response.json();
+    
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML= data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML= data.wind.speed +" Km/h";
+    
+    const weatherIcons = {
+        "Clouds": "images/clouds.png",
+        "Clear": "images/clear.png",
+        "Rain": "images/rain.png",
+        "Drizzle": "images/drizzle.png",
+        "Mist": "images/mist.png",
+        "Snow": "images/snow.png"
+    };
+    
+    weatherIcon.src = weatherIcons[data.weather[0].main];
+
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+        
+    }
+}
+searchbtn.addEventListener("click",()=>{
+checkweather(searchbox.value);
+})
